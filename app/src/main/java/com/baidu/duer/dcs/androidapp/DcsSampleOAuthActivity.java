05c2a70db1d1bbae2982bbec2ba400bdbcf9140c
guxiuzhong <guxiuzhong@baidu.com>
2018-01-03 14:16:37 +0800

    去掉sample的client_id

--- app/src/main/java/com/baidu/duer/dcs/androidapp/DcsSampleOAuthActivity.java
+++ app/src/main/java/com/baidu/duer/dcs/androidapp/DcsSampleOAuthActivity.java
@@ -39,7 +39,7 @@ import com.baidu.duer.dcs.util.LogUtil;
 public class DcsSampleOAuthActivity extends Activity implements View.OnClickListener {
     // 需要开发者自己申请client_id
     // client_id，就是oauth的client_id
-    private static final String CLIENT_ID = "d8ITlI9aeTPaGcxKKsZit8tq";
+    private static final String CLIENT_ID = "{{placeholder for your client_id}}";
     // 是否每次授权都强制登陆
     private boolean isForceLogin = false;
     // 是否每次都确认登陆
